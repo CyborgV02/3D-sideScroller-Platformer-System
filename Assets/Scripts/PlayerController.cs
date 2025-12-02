ff5a3ba02f9a9360e01906cde3e464b3907b6e39
Cyborg <ahmedkamalv2@gmail.com>
2025-12-02 08:57:31 +0200

    fixed the floating jump bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    public float speed=5.0f;
+   public float jumpForce=5.0f;
    private Rigidbody rb ;
     void Start()
     {
@@ -12,8 +13,23 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+      
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump();
+        }
+    }
+
+    void jump()
+    {
+        rb.AddForce(Vector3.up*jumpForce,ForceMode.Impulse);
+    }
+
+    void FixedUpdate()
+    {
+         float horizontalInput = Input.GetAxis("Horizontal");
        Vector3 movement = new Vector3(0,0,-horizontalInput);
-       rb.MovePosition(transform.position +movement * speed*Time.deltaTime);
+       rb.MovePosition(rb.position+movement * speed*Time.fixedDeltaTime);
     }
 }
