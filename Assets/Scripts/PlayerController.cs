07622e8c860eed166a08b6570e5708fc5ef25b6a
Cyborg <ahmedkamalv2@gmail.com>
2025-12-07 16:41:11 +0200

    added dash mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEditor.Callbacks;
 using UnityEngine;
 
@@ -6,6 +8,15 @@ public class PlayerController : MonoBehaviour
    public float speed=5.0f;
    public float jumpForce=5.0f;
    private Rigidbody rb ;
+   public bool isGrounded=false;
+   public bool canDash=true;
+   public bool isDashing=false;
+   public float dashSpeed=7.0f;
+   public float dashDuration=0.3f;
+   public float dashCooldown=0.5f;
+  
+    
+
     void Start()
     {
         rb=GetComponent<Rigidbody>();
@@ -15,9 +26,14 @@ public class PlayerController : MonoBehaviour
     {
       
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&&isGrounded)
         {
             jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)&&canDash)
+        {
+           StartCoroutine(dash());
         }
     }
 
@@ -29,7 +45,41 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
          float horizontalInput = Input.GetAxis("Horizontal");
-       Vector3 movement = new Vector3(0,0,-horizontalInput);
+       Vector3 movement = new Vector3(horizontalInput,0,0);
        rb.MovePosition(rb.position+movement * speed*Time.fixedDeltaTime);
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded=true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded=false;
+        }
+    }
+
+    private IEnumerator dash()
+    {
+        isDashing=true;
+        canDash=false;
+        rb.useGravity=false;
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        Vector3 movement = new Vector3(horizontalInput,0,0);
+        rb.AddForce(movement * dashSpeed, ForceMode.VelocityChange);
+        yield return new WaitForSeconds(dashDuration);
+        rb.linearVelocity=Vector3.zero;
+        isDashing=false;
+        rb.useGravity=true;
+         canDash=true;
+        yield return new WaitForSeconds(dashCooldown);
+       
+    }
+
 }
